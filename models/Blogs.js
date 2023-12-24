d9ace30ace8f6a9a81dce08dcb2a054630ed4c53
Evan <evanbennett83@gmail.com>
2023-12-24 18:36:37 -0500

    Added Blog model

--- models/Blogs.js
+++ models/Blogs.js
@@ -1,6 +1,7 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/connection');
 
-class Blogs extends Model {}
+class Blogs extends Model { }
 
 Blogs.init(
   {
@@ -10,5 +11,28 @@ Blogs.init(
       allowNull: false,
       autoIncrement: true,
     },
-  }
+    blogTitle: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    blogContent: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    authorId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Users,
+        key: 'temp - MUST CHANGE'// fill in key here
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogs',
+  },
 )
